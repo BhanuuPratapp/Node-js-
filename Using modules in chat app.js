90bdb5452ff13c1797aa144c6aad0baf53ccffe6
BhanuuPratapp <103886926+BhanuuPratapp@users.noreply.github.com>
2022-10-15 18:48:14 +0530

    Create Using modules in chat app.js

---
+++ Using modules in chat app.js
@@ -0,0 +1,66 @@
+const http  = require('http');
+const fs = require('fs');
+const routes = require('./routes');
+
+const server = http.createServer(routes.handler)
+
+server.listen(4000)
+//////////////////////////////////////////
+
+const fs = require('fs');
+
+const eventhandler=(req, res) =>{
+    const url = req.url;
+    const method = req.method;
+    if(url ==='/' )
+    {
+       // const filepath = path.join(__dirname, "message.txt")
+        fs.readFile("message.txt", {encoding: "utf-8"},(err, data)=>{
+            if(err){
+               console.log(err);
+         }
+       console.log(data);
+        res.write('<html>')
+        res.write('<head><title>Enter a message</title></head>')
+        res.write(`<body>${data}</body>`)
+        res.write('<body> <form action ="/message" method="POST"><input type="text" name = "message"></input> <button type ="submit">Send</button></form></body>')
+        res.write('</html')
+        return res.end();
+       })
+    }
+    else if(url === "/message" && method ==="POST")
+    {
+        const body = [];
+        req.on('data',(chunk) =>{
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', ()=>{
+            const parsebody = Buffer.concat(body).toString();
+            const message = parsebody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            res.statusCode = 302
+            res.setHeader('Location','/');
+            return res.end();
+        })
+        
+        
+    }
+    else{
+    console.log(req.url, req.headers,req.method)
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>welcome to home page</title></head>')
+    res.write('<body><h1>Heloo from my nodejs server</h1></body>')
+    res.write('</html>')
+    res.end();
+    }
+}
+//module.exports = eventhandler
+//module.exports= {
+ //   handler : eventhandler,
+  //  text: 'some hard code '
+//};
+
+//module.exports.handler = eventhandler
+exports.handler = eventhandler
